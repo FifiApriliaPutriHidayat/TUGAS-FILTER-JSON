61296752faee98a29a7217558fcc81302c551e57
FifiApriliaPutriHidayat <134756699+FifiApriliaPutriHidayat@users.noreply.github.com>
2024-10-14 20:59:26 +0700

    Add files via upload

---
+++ pemrograman_api/index.ts
@@ -0,0 +1,39 @@
+let data = [
+	{
+		"toko": {
+			"nama" : "Toko Serba Ada",
+			"alamat" : "Jl.Kalimantan No. 12, Blitar",
+			"nomor telepon" : "022-1234567",
+			"produk" : [
+				{
+					"id": 1,
+					"nama" : "Sabun Mandi",
+					"kategori" : "Kecantikan",
+					"stok" : "51",
+					"harga" : "15000",
+				},
+				{
+					"id": 2,
+					"nama" : "Shampo",
+					"kategori" : "Perawatan",
+					"stok" : "38",
+					"harga" : "9500",
+				},
+				{
+					"id": 3,
+					"nama" : "Pasta Gigi",
+					"kategori" : "Kesehatan",
+					"stok" : "46",
+					"harga" : "8750",
+				},
+				{
+					"id": 4,
+					"nama" : "Lulur Badan",
+					"kategori" : "Kecantikan",
+					"stok" : "51",
+					"harga" : "27000",
+				},
+			]
+		}
+	}
+]
